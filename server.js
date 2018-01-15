18cd7fe3be7db03188f9e4c50e125d3bcddbbfbc
Glitch (file-metada) <none>
2018-01-15 19:49:51 +0000

    🕋🌝 Checkpoint views/index.html:553280/132 public/style.css:553280/67 server.js:553280/4

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ var app = express();
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+// http://expressjs.com/en/starter/static-files.html
+app.use('/public', express.static(process.cwd() + '/public'));
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
