8f112ff77d79d8706c6eb86dd0e86ebfae73c6e5
Glitch (file-metada) <none>
2018-01-16 16:15:56 +0000

    🚛🍠 Checkpoint server.js:553280/277 public/client.js:553280/138

--- public/client.js
+++ public/client.js
@@ -16,6 +16,32 @@ $(function() {
   $('form').submit(function(event) {
     event.preventDefault();
     var dream = $('input').val();
+    $.post('/uploads?' + $.param({uploads: dream}), function() {
+      $('<p></p>').text(dream).appendTo('ul#dreams');
+      $('input').val('');
+      $('input').focus();
+    });
+  });
+
+});
+
+/*
+$('.btn').on('click', function() {
+  $('#upload').click();
+});
+
+$(function() {
+  console.log('hello world :o');
+  
+  $.get('/dreams', function(dreams) {
+    dreams.forEach(function(dream) {
+      $('<li></li>').text(dream).appendTo('ul#dreams');
+    });
+  });
+
+  $('form').submit(function(event) {
+    event.preventDefault();
+    var dream = $('input').val();
     $.post('/dreams?' + $.param({dream: dream}), function() {
       console.log(dream)
       $('<p></p>').text(dream).appendTo('ul#dreams');
@@ -26,8 +52,7 @@ $(function() {
 
 });
 
-/*
-$('.bttn').on('click', function() {
-  $('#upload').click();
-});
+
+
+
 */
--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var formidable = require('formidable');
 var express = require('express');
+var util = require('util');
 var app = express();
 
 // http://expressjs.com/en/starter/static-files.html
@@ -11,7 +12,7 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use('/public', express.static(__dirname + '/public'));
-console.log(process.cwd())
+//console.log(process.cwd())
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
@@ -22,12 +23,31 @@ app.get("/dreams", function (request, response) {
   console.log('request parameter', request.params, 'upload', upload)
   response.send(upload);
 });*/
-
+/*
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
 app.post("/dreams", function (request, response) {
   upload.push(request.query.dream);
   console.log(request.query.dream)
   response.sendStatus(200);
+});*/
+
+app.put("/uploads", function (req, res) {
+  
+      var form = new formidable.IncomingForm();
+      form.uploadDir = '/uploads';
+      form
+ 
+    form.parse(req, function(err, fields, files) {
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('received upload:\n\n');
+      res.end(util.inspect({fields: fields, files: files}));
+    });
+ 
+    return;
+  
+  upload.push(req.query.uploads);
+  console.log(req)
+  res.sendStatus(200);
 });
 
 // Simple in-memory store for now
